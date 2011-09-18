2e7d0cd7629b9d8ae3d4e723218eb433977a470c
ejurupe@gmail.com <ejurupe@gmail.com@30fac3dd-1828-a9e7-39b4-11dbce07c668>
2011-09-18 18:06:47 +0000

    Creación de Agencia

--- trunk/VentasPasajes/src/main/java/com/venta/pasajes/model/Agencia.java
+++ trunk/VentasPasajes/src/main/java/com/venta/pasajes/model/Agencia.java
@@ -1,5 +1,28 @@
 package com.venta.pasajes.model;
 
 public class Agencia {
+	
+	private int idAgencia;
+	private String nombre;
+	private String direccion;
+	
+	public int getIdAgencia() {
+		return idAgencia;
+	}
+	public void setIdAgencia(int idAgencia) {
+		this.idAgencia = idAgencia;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 
 }
