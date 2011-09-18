03abb31ecd1fdaa2e64cd3313349aa6440e6b810
ejurupe@gmail.com <ejurupe@gmail.com@30fac3dd-1828-a9e7-39b4-11dbce07c668>
2011-09-18 18:15:50 +0000

    beans

--- trunk/VentasPasajes/src/main/java/com/venta/pasajes/model/Bus.java
+++ trunk/VentasPasajes/src/main/java/com/venta/pasajes/model/Bus.java
@@ -1,5 +1,36 @@
 package com.venta.pasajes.model;
 
 public class Bus {
+	
+	private int idBus;
+	private String placa;
+	private int numAsiento;
+	private int estado;
+	
+	public int getIdBus() {
+		return idBus;
+	}
+	public void setIdBus(int idBus) {
+		this.idBus = idBus;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public int getNumAsiento() {
+		return numAsiento;
+	}
+	public void setNumAsiento(int numAsiento) {
+		this.numAsiento = numAsiento;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	
 
 }
--- trunk/VentasPasajes/src/main/java/com/venta/pasajes/model/Usuario.java
+++ trunk/VentasPasajes/src/main/java/com/venta/pasajes/model/Usuario.java
@@ -1,20 +1,64 @@
 package com.venta.pasajes.model;
 
 public class Usuario {
-	private String user;
-	private String nombre;
+	private String nomUsuario;
+	private String apepatUsuario;
+	private String apematUsuario;
+	private int tipoDocumento;
+	private String numDocumento;
+	private int sexo;
+	private String email;
+	private String password;
 	
-	public String getUser() {
-		return user;
+	public String getNomUsuario() {
+		return nomUsuario;
 	}
-	public void setUser(String user) {
-		this.user = user;
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
 	}
-	public String getNombre() {
-		return nombre;
+	public String getApepatUsuario() {
+		return apepatUsuario;
 	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public void setApepatUsuario(String apepatUsuario) {
+		this.apepatUsuario = apepatUsuario;
 	}
+	public String getApematUsuario() {
+		return apematUsuario;
+	}
+	public void setApematUsuario(String apematUsuario) {
+		this.apematUsuario = apematUsuario;
+	}
+	public int getTipoDocumento() {
+		return tipoDocumento;
+	}
+	public void setTipoDocumento(int tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+	public String getNumDocumento() {
+		return numDocumento;
+	}
+	public void setNumDocumento(String numDocumento) {
+		this.numDocumento = numDocumento;
+	}
+	public int getSexo() {
+		return sexo;
+	}
+	public void setSexo(int sexo) {
+		this.sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 	
 }
