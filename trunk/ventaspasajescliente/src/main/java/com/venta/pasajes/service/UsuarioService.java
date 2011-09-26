27c16c9e1ce5bd63e163796fa0d69a276f35e836
jroa3000@gmail.com <jroa3000@gmail.com@30fac3dd-1828-a9e7-39b4-11dbce07c668>
2011-09-26 06:47:26 +0000

    Venta Pasajes Cliente

---
+++ trunk/ventaspasajescliente/src/main/java/com/venta/pasajes/service/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.venta.pasajes.service;
+
+import com.venta.pasajes.model.Usuario;
+
+public interface UsuarioService {
+	public Usuario registrarUsuario(Usuario usuario);
+	public void eliminarUsuario(Usuario usuario);
+	public Usuario buscarUsuario(String codUsuario, String password);
+	public void actualizarUsuario(Usuario usuario);
+
+}
