69da33da1f07e4f4fbe282ec1dce3cacf9468710
jroa3000@gmail.com <jroa3000@gmail.com@30fac3dd-1828-a9e7-39b4-11dbce07c668>
2011-09-30 03:44:34 +0000

    Logeo terminado

---
+++ trunk/ventaspasajescliente/src/main/java/com/venta/pasajes/service/UsuarioServiceClientImpl.java
@@ -0,0 +1,44 @@
+package com.venta.pasajes.service;
+
+import com.venta.pasajes.model.Usuario;
+
+public class UsuarioServiceClientImpl implements UsuarioServiceClient{
+
+	@Override
+	public Usuario registrarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Usuario buscarUsuario(String codUsuario, String password) {
+		Usuario usuario = null;
+
+		if(codUsuario.equals("JROA") && password.equals("123456"))
+		{
+			usuario = new Usuario();
+			usuario.setCodUsuario("JROA");
+			usuario.setNomUsuario("JONATHAN");
+			usuario.setApepatUsuario("ROA");
+			usuario.setApematUsuario("CANSAYA");
+			usuario.setTipoDocumento(1);
+			usuario.setNumDocumento("42364208");
+			usuario.setSexo(1);
+			usuario.setEmail("jroa_3000@hotmail.com");
+			usuario.setPassword("123456");
+		}
+		return usuario;
+	}
+
+	@Override
+	public void actualizarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
+}
